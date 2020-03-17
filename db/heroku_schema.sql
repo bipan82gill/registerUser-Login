804bd7743f7df821191aba74aaecf3c25c2bf6e8
bipan82gill <56560207+bipan82gill@users.noreply.github.com>
2020-03-17 18:53:22 -0400

    adding new field to database

--- db/heroku_schema.sql
+++ db/heroku_schema.sql
@@ -4,12 +4,12 @@ CREATE DATABASE x589dfj36f9mdef0;
 USE x589dfj36f9mdef0;
 
 CREATE TABLE users(
-      id int AUTO_INCREMENT,
+     id int AUTO_INCREMENT NOT NULL,
       username VARCHAR(20),
       fullname VARCHAR(20),
       password VARCHAR(128),
+      createdAt TIMESTAP NOT NULL,
       PRIMARY KEY(id));
-
 INSERT INTO users(username, fullname, password)
 VALUES('bipangill',"bipanjeet gill", 'japleen1234');
 
--- db/schema.sql
+++ db/schema.sql
@@ -3,8 +3,9 @@ CREATE DATABASE logindb;
 USE logindb;
  
 CREATE TABLE users(
-      id int AUTO_INCREMENT,
+      id int AUTO_INCREMENT NOT NULL,
       username VARCHAR(20),
       fullname VARCHAR(20),
       password VARCHAR(128),
+      createdAt TIMESTAP NOT NULL,
       PRIMARY KEY(id));
